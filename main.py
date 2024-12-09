164d357d178ede87c4c52b27f8a99ade028e3776
mottzerella <154298358+mottzerella@users.noreply.github.com>
2024-12-09 10:19:53 -0800

    Update main.py

--- main.py
+++ main.py
@@ -217,7 +217,7 @@ if __name__ == "__main__":
 
     #st.image('')
     st.header('Welcome to Mattbot! :party')
-    st.subheader('I use an Agentic RAG workflow under the hood. Be friendly, I'm still in beta')
+    st.subheader("I use an Agentic RAG workflow under the hood. Be friendly, I'm still in beta :smile")
    #with st.sidebar:
     
     chain = get_multi_doc_chain()
